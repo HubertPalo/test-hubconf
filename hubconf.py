30c5aad2e55d02dccde2a56d5d354088deaed102
HubertPalo <darlinnep@gmail.com>
2026-08-08 20:12:28 +0000

    dynamic functions

--- hubconf.py
+++ hubconf.py
@@ -1,14 +1,20 @@
 from minerva.models.nets.lfr_har_architectures import HARSCnnEncoder
+import yaml
 
+def _create_model(model_name: str, link: str):
+    print(f"Creating model: {model_name}, Link: {link}")
+    return "asd"
+# def _create_model(pretrained=False):
+#     return HARSCnnEncoder(
+#         dim=125,
+#         input_channel=6,
+#         inner_conv_output_dim=128 * 10
+#     )
 
-def _create_model(pretrained=False):
-    return HARSCnnEncoder(
-        dim=125,
-        input_channel=6,
-        inner_conv_output_dim=128 * 10
-    )
+available_models = yaml.safe_load(open("links.yaml", "r"))
+for model_name, link in available_models.items():
+    globals()[model_name] = _create_model(model_name, link)
 
+# MODEL_NAME = "harsccencoder"
 
-MODEL_NAME = "harsccencoder"
-
-globals()[MODEL_NAME] = _create_model
+# globals()[MODEL_NAME] = _create_model
